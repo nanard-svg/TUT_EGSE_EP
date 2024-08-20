cb7228a639b455e6096c3df81b801b95e6123f08
bbertrand-eng <bernard.bertrand@irap.omp.eu>
2024-08-20 11:05:40 +0200

    +script filter_plote.py + in_file_divider by eight_out_file.py script

---
+++ software/filter_plote.py
@@ -0,0 +1,46 @@
+import ok
+import time
+import array as arr
+
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+count = 0
+count_data = 0
+ligne = 0
+i = 0
+indice=1
+lignes = []
+list_array_pipe_out_MSB = []
+list_array_pipe_out_LSB = []
+param_vals=0
+
+
+file_names = ['1_MSB.txt','2_MSB.txt','3_MSB.txt','4_MSB.txt','5_MSB.txt','6_MSB.txt']
+
+for file_name in file_names:
+#################################### read file from list name ##########################################
+    file_name = open(file_name, "r")
+    lines = file_name.readlines()
+    formated_lines = []
+    for elm in lines:
+        formated_lines.append(int(elm[:-1]))
+    print("############################################")
+    print("file name fichier lue {}".format(file_name))
+    #print("print formated lines {}".format(formated_lines)) #### print liste ONE file
+    #print("min ", min(formated_lines))
+    print("max ", max(formated_lines))
+    #print("max-min", max(formated_lines)-min(formated_lines))
+    formated_lines.remove(max(formated_lines))
+    print("second largest",max(formated_lines))
+    formated_lines.remove(max(formated_lines))
+    print("third largest",max(formated_lines))
+
+    plt.subplot(221)
+    plt.plot(formated_lines)
+    plt.title("Filter")
+    plt.xlabel("temps")
+    plt.ylabel("amplitude")
+
+    plt.show()
---
+++ software/in_file_divider by eight_out_file.py
@@ -0,0 +1,39 @@
+import numpy as np
+import math
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+print ("divider by eight")
+file_name_out = f"in_file.txt"
+file = open(file_name_out, "r")
+lines = file.readlines()
+
+formated_lines_before = []
+formated_lines_hex_before = []
+
+for elm in lines :
+    formated_lines_before.append((int(elm[:-1])))##la liste lines a des eleementr ascii dont on supprime\n avec :-1
+    #print(formated_lines_before)
+
+for elm in formated_lines_before :
+    formated_lines_hex_before.append((round(elm/8)))
+    #print(formated_lines_hex_before)
+
+file_name_out = f"out_file.txt"
+file = open(file_name_out, "w")
+for items in formated_lines_hex_before:
+    file.write('%s\n' % items)
+file.close()
+
+plt.subplot(211)
+plt.plot(formated_lines_before)
+plt.title("in filter")
+plt.xlabel("temps")
+plt.ylabel("amplitude")
+plt.subplot(212)
+plt.plot(formated_lines_hex_before)
+plt.title("out filter")
+plt.xlabel("temps")
+plt.ylabel("amplitude")
+plt.show()
